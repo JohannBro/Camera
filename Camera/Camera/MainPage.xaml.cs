6741cb1fcd55c8f7d0f1f93b8d953cb990de7b92
JB <johann.brochier2019@campus-eni.fr>
2019-09-29 17:18:35 +0200

    Ajoutez des fichiers projet.

---
+++ Camera/Camera/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Camera
+{
+    
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        async private void Photo_Clicked(object sender, EventArgs e)
+        {
+            this.bouton.IsEnabled = false;
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable ||
+                !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("Pas de caméra", ": (Pas de caméra disponible.", "OK");
+                return;
+            }
+            var file = await CrossMedia.Current.TakePhotoAsync(
+               new Plugin.Media.Abstractions.StoreCameraMediaOptions
+               {
+                   Directory = "my_images",
+                   Name = "Ma photo.jpg"
+                   
+               });
+            if (file == null)
+                return;
+            await DisplayAlert("Emplacement de la photo : ", file.Path, "OK");
+
+            PhotoImage.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+    }
+}
